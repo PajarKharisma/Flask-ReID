675dd49c567f9d8c279bdc2cc05b43434f92450e
PajarKharisma <pajarkharisma@hotmail.com>
2020-08-23 11:52:27 +0700

    fix resize

--- core/camera.py
+++ core/camera.py
@@ -2,6 +2,7 @@ import cv2
 ds_factor=0.6
 
 import os
+import imutils
 
 class VideoCamera(object):
     def __init__(self, url=1):
@@ -12,6 +13,6 @@ class VideoCamera(object):
     
     def get_frame(self):
         success, image = self.video.read()
-        image=cv2.resize(image,None,fx=ds_factor,fy=ds_factor,interpolation=cv2.INTER_AREA)
+        image = imutils.resize(image, width=960)
         ret, jpeg = cv2.imencode('.jpg', image)
         return jpeg.tobytes()
